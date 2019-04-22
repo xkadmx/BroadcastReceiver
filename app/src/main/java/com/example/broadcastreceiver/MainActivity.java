2492a2f8a1d25c71c391f6793d517c75da9570d4
xkadmx <zuzanna.rosinska@yahoo.com>
2019-04-22 19:23:19 +0200

    registering broadcast and filter objects in onPostResume activity of application lifecycle method

--- app/src/main/java/com/example/broadcastreceiver/MainActivity.java
+++ app/src/main/java/com/example/broadcastreceiver/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.broadcastreceiver;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private IntentFilter filter = new IntentFilter("android.provider.Telephony.SMS_RECEIVER");
+    private BroadcastReceiver broadcast = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Toast.makeText(context,"Masz wiadomosc", Toast.LENGTH_SHORT).show();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        registerReceiver(broadcast, filter);
     }
 }
