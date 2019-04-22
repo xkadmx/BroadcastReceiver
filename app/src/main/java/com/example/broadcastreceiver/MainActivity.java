3cd141e1c66e8c14d8e4a6a9b361a2ae46832311
xkadmx <zuzanna.rosinska@yahoo.com>
2019-04-22 20:22:01 +0200

    because we use modern app we need to receive approval from the user confirming he agrees to inform the app about receiving SMS (apart from permission given in AndroidManifest)so we used onRequestPermissionResult which obtains questionId declared as private int

--- app/src/main/java/com/example/broadcastreceiver/MainActivity.java
+++ app/src/main/java/com/example/broadcastreceiver/MainActivity.java
@@ -5,9 +5,11 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,8 +27,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ActivityCompat.requestPermissions(this, String[], {
-            Manifest.permission.RECEIVE_SMS), questionId};
+        ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.RECEIVE_SMS}, questionId);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode==questionId){
+            Log.i("Permission", "Mamy pozwolenie!");
+        }
     }
 
     @Override
