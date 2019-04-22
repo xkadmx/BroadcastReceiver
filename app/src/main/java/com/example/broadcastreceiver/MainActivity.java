453202967e7451ca5d964ef817c85d3e3dce5e8d
xkadmx <zuzanna.rosinska@yahoo.com>
2019-04-22 19:59:55 +0200

    in onCreate() we create ActivityCompat method to request permission to receive notification about receiving SMS

--- app/src/main/java/com/example/broadcastreceiver/MainActivity.java
+++ app/src/main/java/com/example/broadcastreceiver/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.broadcastreceiver;
 
+import android.Manifest;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private int questionId = 5;
 
     private IntentFilter filter = new IntentFilter("android.provider.Telephony.SMS_RECEIVER");
     private BroadcastReceiver broadcast = new BroadcastReceiver() {
@@ -22,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActivityCompat.requestPermissions(this, String[], {
+            Manifest.permission.RECEIVE_SMS), questionId};
     }
 
     @Override
