7a72142b843e72c5b5cc935a2d49795e80be9a89
xkadmx <zuzanna.rosinska@yahoo.com>
2019-04-22 21:02:10 +0200

    adding SampleReceiver class extending BroadcastReceiver interface, creating method onReceive to inform user (with a Toast) he received SMS

---
+++ app/src/main/java/com/example/broadcastreceiver/SampleReceiver.java
@@ -0,0 +1,15 @@
+package com.example.broadcastreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class SampleReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "Nowy sms z manifestu", Toast.LENGTH_SHORT).show();
+
+    }
+}
